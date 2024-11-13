8b9861e1c388532e0752491e28a540f2f77047d7
Joan <joan@gmail.com>
2024-11-13 19:43:40 +0100

    404 response

--- activitat8/main.py
+++ activitat8/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+
+class Usuari(BaseModel):
+    nom: str
+    cognom: str | None = None
+    salari: float
+    edat: int | None = None
 
 app = FastAPI()
 
+@app.get("/usuaris/{nom}")
+async def get_usuari(nom:str,cognom:str,salari:float,edat:int,response:Response):
+    usuari = {"nom":nom,"cognom":cognom,"salari":salari,"edat":edat}
+    if usuari and nom=="":
+        response.status_code = 404
+        return {"Error":"Usuari no trobat"}
+    return usuari
 
-@app.get("/")
-async def root():
-    return {"Missatge": "Fast API de Joan","Versio":"En proves","Data":"11/11/2024"}
-
-@app.post("/post/{edat}")
-async def post(edat:int):
-    return {"La teva edat es":edat}
+@app.post("/usuaris/")
+async def crea_usuari(usuari: Usuari):
+    return usuari
